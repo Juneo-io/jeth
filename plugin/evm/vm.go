b4cc504c96b3c6ba84e1784a1df2ff713f3f5983
alekswaslet <1346150+alekswaslet@users.noreply.github.com>
2024-04-20 10:40:49 +0200

    State sync default state to disabled

--- plugin/evm/vm.go
+++ plugin/evm/vm.go
@@ -2076,5 +2076,7 @@ func (vm *VM) stateSyncEnabled(lastAcceptedHeight uint64) bool {
 	}
 
 	// enable state sync by default if the chain is empty.
-	return lastAcceptedHeight == 0
+	// return lastAcceptedHeight == 0
+	// disable it by default for the start of the network
+	return false
 }
